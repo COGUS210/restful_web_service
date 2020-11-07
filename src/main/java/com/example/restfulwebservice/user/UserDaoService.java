f26abc08d2e998b2da35811e69252f2b34f43d0c
COGUS210 <43839577+COGUS210@users.noreply.github.com>
2020-11-07 15:16:50 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,62 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    static {
+        users.add(new User(1, "chaihyun", new Date(), "pass1", "980210-2222222"));
+        users.add(new User(2, "cnainvun", new Date(), "pass2", "980210-2222222"));
+        users.add(new User(3, "CHAIHYUN", new Date(), "pass3", "980210-2222222"));
+    }
+
+    // 전체 사용자 조회
+    public List<User> findAll() {
+        return users;
+    }
+
+    // 사용자 추가
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    // 개별 사용자 조회
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // 사용자 삭제
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while(iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
